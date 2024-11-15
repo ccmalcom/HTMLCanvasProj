c279cda2c2c72ec7b76b7b75cf40a8e7c2b74cc0
ccmalcom <chasecmalcom@gmail.com>
2024-11-15 16:57:11 -0500

    Game object complete

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-import { GameLoop } from './src/GameLoop';
-import { resources } from './src/Resources';
-import { Sprite } from './src/Sprite';
-import { Vector2 } from './src/Vector2';
-import { DOWN, UP, LEFT, Input, RIGHT } from './src/Input';
 import './style.css'
-import { gridCells, isSpaceFree } from './src/helpers/grid';
-import { moveTowards } from './src/helpers/moveTowards';
-import { walls } from './src/levels/level1';
-import { Animations } from './src/Animations';
-import { FrameIndexPattern } from './src/FrameIndexPattern';
-import { STAND_DOWN, STAND_UP, STAND_LEFT, STAND_RIGHT, WALK_DOWN, WALK_LEFT, WALK_RIGHT, WALK_UP } from './src/objects/Hero/heroAnimations';
-import { GameObject } from './src/GameObject';
+import { resources } from "./src/Resource.js";
+import { Sprite } from "./src/Sprite.js";
+import { Vector2 } from "./src/Vector2.js";
+import { GameLoop } from "./src/GameLoop.js";
+import { Input } from "./src/Input.js";
+import { gridCells } from "./src/helpers/grid.js";
+import { GameObject } from "./src/GameObject.js";
+import { Hero } from "./src/objects/Hero/Hero.js";
 
+
+//get the canvas and context
 const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
 
+//create the main scene
 const mainScene = new GameObject({
   position: new Vector2(0, 0)
 });
 
+//add the sky and ground sprites
 //todo: move to a separate file when more than one level
 const skySprite = new Sprite({
   resource: resources.images.sky,
@@ -30,113 +30,23 @@ const groundSprite = new Sprite({
   frameSize: new Vector2(320, 180)
 });
 mainScene.addChild(groundSprite);
-const hero = new Sprite({
-  resource: resources.images.hero,
-  frameSize: new Vector2(32, 32),
-  position: new Vector2(gridCells(6), gridCells(5)),
-  hFrames: 3,
-  vFrames: 8,
-  frame: 1,
-  animations: new Animations({
-    walkDown: new FrameIndexPattern(WALK_DOWN),
-    walkUp: new FrameIndexPattern(WALK_UP),
-    walkLeft: new FrameIndexPattern(WALK_LEFT),
-    walkRight: new FrameIndexPattern(WALK_RIGHT),
-    standDown: new FrameIndexPattern(STAND_DOWN),
-    standUp: new FrameIndexPattern(STAND_UP),
-    standLeft: new FrameIndexPattern(STAND_LEFT),
-    standRight: new FrameIndexPattern(STAND_RIGHT),
-  })
 
-});
+const hero = new Hero(gridCells(6), gridCells(5));
+mainScene.addChild(hero);
 
-const heroDestinationPosition = hero.position.duplicate();
+//create the input object
+mainScene.input = new Input();
 
-let heroFacing = DOWN;
-const shadow = new Sprite({
-  resource: resources.images.shadow,
-  frameSize: new Vector2(32, 32),
-});
-
-const input = new Input();
-
+//establish update and draw loops
 const update = (delta) => {
-  console.log('delta', delta);
-  const distance = moveTowards(hero, heroDestinationPosition, 1);
-  const hasArrived = distance <= 1;
-  if (hasArrived) {
-    tryMove();
-  }
-
-  //work on here animations
-  hero.step(delta)
-
-  return;
+  mainScene.stepEntry(delta, mainScene);
 
 }
-const tryMove = () => {
 
-  if (!input.direction) {
-    switch (heroFacing) {
-      case UP:
-        hero.animations.play('standUp');
-        break;
-      case DOWN:
-        hero.animations.play('standDown');
-        break;
-      case LEFT:
-        hero.animations.play('standLeft');
-        break;
-      case RIGHT:
-        hero.animations.play('standRight');
-        break;
-    }
-    return;
-  }
-  let nextX = heroDestinationPosition.x;
-  let nextY = heroDestinationPosition.y;
-  const gridSize = 16;
-  switch (input.direction) {
-    case UP:
-      nextY -= gridSize;
-      hero.animations.play('walkUp');
-      break;
-    case DOWN:
-      nextY += gridSize;
-      hero.animations.play('walkDown');
-      break;
-    case LEFT:
-      nextX -= gridSize;
-      hero.animations.play('walkLeft');
-      break;
-    case RIGHT:
-      nextX += gridSize;
-      hero.animations.play('walkRight');
-      break;
-  }
-  heroFacing = input.direction ?? heroFacing;
-
-  if (isSpaceFree(walls, nextX, nextY)) {
-    heroDestinationPosition.x = nextX;
-    heroDestinationPosition.y = nextY;
-  }
-
-}
 const draw = () => {
-
   mainScene.draw(ctx, 0, 0);
-
-  // skySprite.drawImage(ctx, 0, 0);
-  // groundSprite.drawImage(ctx, 0, 0);
-
-  // // Center the Hero in the cell
-  // const heroOffset = new Vector2(-8, -21);
-  // const heroPosX = hero.position.x + heroOffset.x;
-  // const heroPosY = hero.position.y + 1 + heroOffset.y;
-
-  // shadow.drawImage(ctx, heroPosX, heroPosY);
-  // hero.drawImage(ctx, heroPosX, heroPosY);
 }
 
+//start the game loop
 const gameLoop = new GameLoop(update, draw);
 gameLoop.start();
--- src/objects/Hero/Hero.js
+++ src/objects/Hero/Hero.js
@@ -1,10 +1,109 @@
-import { GameObject } from "../../GameObject";
+import { GameObject } from "../../GameObject.js";
+import { Vector2 } from "../../Vector2.js";
+import { DOWN, LEFT, RIGHT, UP } from "../../Input.js";
+import { isSpaceFree } from "../../helpers/grid.js";
+import { walls } from "../../levels/level1.js";
+import { Sprite } from "../../Sprite.js";
+import { resources } from "../../Resource.js";
+import { Animations } from "../../Animations.js";
+import { FrameIndexPattern } from "../../FrameIndexPattern.js";
+import { gridCells } from "../../helpers/grid.js";
+import { WALK_DOWN, WALK_UP, WALK_LEFT, WALK_RIGHT, STAND_DOWN, STAND_UP, STAND_LEFT, STAND_RIGHT } from "./HeroAnimations.js";
+import { moveTowards } from "../../helpers/moveTowards.js";
+
+
+
 
 export class Hero extends GameObject {
     constructor(x, y) {
         super({
             position: new Vector2(x, y)
         })
+
+        const shadow = new Sprite({
+            resource: resources.images.shadow,
+            frameSize: new Vector2(32, 32),
+            position: new Vector2(-8, -21),
+        });
+        this.addChild(shadow);
+
+        this.body = new Sprite({
+            resource: resources.images.hero,
+            frameSize: new Vector2(32, 32),
+            position: new Vector2(-8, -20),
+            hFrames: 3,
+            vFrames: 8,
+            frame: 1,
+            animations: new Animations({
+                walkDown: new FrameIndexPattern(WALK_DOWN),
+                walkUp: new FrameIndexPattern(WALK_UP),
+                walkLeft: new FrameIndexPattern(WALK_LEFT),
+                walkRight: new FrameIndexPattern(WALK_RIGHT),
+                standDown: new FrameIndexPattern(STAND_DOWN),
+                standUp: new FrameIndexPattern(STAND_UP),
+                standLeft: new FrameIndexPattern(STAND_LEFT),
+                standRight: new FrameIndexPattern(STAND_RIGHT),
+            })
+        });
+        this.addChild(this.body);
+
+        this.facingDirection = DOWN;
+        this.destinationPosition = this.position.duplicate();
     }
 
+    step(delta, root) {
+        const distance = moveTowards(this, this.destinationPosition, 1);
+        const hasArrived = distance <= 1;
+        if (hasArrived) {
+            this.tryMove(root);
+        }
+    }
+    tryMove(root) {
+        const { input } = root;
+        if (!input.direction) {
+            switch (this.facingDirection) {
+                case UP:
+                    this.body.animations.play('standUp');
+                    break;
+                case DOWN:
+                    this.body.animations.play('standDown');
+                    break;
+                case LEFT:
+                    this.body.animations.play('standLeft');
+                    break;
+                case RIGHT:
+                    this.body.animations.play('standRight');
+                    break;
+            }
+            return;
+        }
+        let nextX = this.destinationPosition.x;
+        let nextY = this.destinationPosition.y;
+        const gridSize = 16;
+        switch (input.direction) {
+            case UP:
+                nextY -= gridSize;
+                this.body.animations.play('walkUp');
+                break;
+            case DOWN:
+                nextY += gridSize;
+                this.body.animations.play('walkDown');
+                break;
+            case LEFT:
+                nextX -= gridSize;
+                this.body.animations.play('walkLeft');
+                break;
+            case RIGHT:
+                nextX += gridSize;
+                this.body.animations.play('walkRight');
+                break;
+        }
+        this.facingDirection = input.direction ?? this.facingDirection;
+
+        if (isSpaceFree(walls, nextX, nextY)) {
+            this.destinationPosition.x = nextX;
+            this.destinationPosition.y = nextY;
+        }
+
+    }
 }
